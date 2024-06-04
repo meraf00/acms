ca706c68715a5ae3c903765ca794d29aac2cec85
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-04 15:18:17 +0300

    feat: add auth controllers

---
+++ acms-be/src/auth/controllers/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+
+import { AuthService } from '../services/auth.service';
+import { GoogleOauthGuard } from '../guards/google-oauth.guard';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('google')
+  @UseGuards(GoogleOauthGuard)
+  async auth() {}
+
+  @Get('google/callback')
+  @UseGuards(GoogleOauthGuard)
+  async googleAuthCallback(@Req() req: Request) {
+    const token = await this.authService.signIn(req.user as unknown as User);
+
+    return token;
+  }
+}
