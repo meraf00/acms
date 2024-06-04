5d552fd859f3dafb68b2c840c81d70bf57fac737
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-04 18:35:22 +0300

    feat: add role guard on contest controller

--- acms-be/src/contest/controllers/contest.controller.ts
+++ acms-be/src/contest/controllers/contest.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { ContestService } from '../services/contest.service';
 import {
   CreateContestDto,
@@ -11,6 +11,9 @@ import { EntityController } from 'src/shared/controllers/entity.controller';
 import { Contest } from '../entities/contest.entity';
 import { ApiTags } from '@nestjs/swagger';
 import { EntityControllerOptions } from '@shared/types/controller-options';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { Roles } from '@shared/types/roles';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 const controllerOptions: EntityControllerOptions = {
   createSchema: createContestSchema,
@@ -21,6 +24,8 @@ const controllerOptions: EntityControllerOptions = {
 
 @ApiTags('contests')
 @Controller('contests')
+@UseGuards(RoleGuard([Roles.hoe, Roles.hoe, Roles.acms]))
+@UseGuards(JwtAuthGuard)
 export class ContestController extends EntityController<Contest>(
   controllerOptions,
 ) {
