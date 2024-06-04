a130d103438ce3d12911ee46095b655c17c87a4d
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-04 15:17:52 +0300

    feat: add auth service

---
+++ acms-be/src/auth/services/auth.service.ts
@@ -0,0 +1,62 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterUserDto } from '../dtos/requests.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/user/entities/user.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  generateJwt(payload: any) {
+    return this.jwtService.sign(payload);
+  }
+
+  async signIn(user: User) {
+    if (!user) {
+      throw new BadRequestException('auth_unauthenticated');
+    }
+
+    const userExists = await this.findUserByEmail(user.email);
+
+    if (!userExists) {
+      return this.registerUser(user);
+    }
+
+    return this.generateJwt({
+      sub: userExists.email,
+      email: userExists.email,
+    });
+  }
+
+  async registerUser(user: RegisterUserDto) {
+    try {
+      const newUser = await this.userModel.create(user);
+
+      return this.generateJwt({
+        sub: newUser.id,
+        email: newUser.email,
+      });
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async findUserByEmail(email: string): Promise<User> {
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
