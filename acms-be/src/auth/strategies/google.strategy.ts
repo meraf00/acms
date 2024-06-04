0728d128388e2a296638cd51fb5ad8505c71299f
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-04 15:17:26 +0300

    feat: add jwt and google strategies

---
+++ acms-be/src/auth/strategies/google.strategy.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+
+import { Strategy, VerifyCallback } from 'passport-google-oauth2';
+import { OAuthConfig } from 'src/config';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      clientID: configService.get<OAuthConfig>('oauth').client_id,
+      clientSecret: configService.get<OAuthConfig>('oauth').client_secret,
+      callbackURL: configService.get<OAuthConfig>('oauth').redirect_uris[0],
+      scope: ['profile', 'email'],
+    });
+  }
+
+  async validate(
+    _accessToken: string,
+    _refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { id, name, emails, photos } = profile;
+
+    console.log(profile);
+
+    const user = {
+      provider: 'google',
+      providerId: id,
+      email: emails[0].value,
+      name: `${name.givenName} ${name.familyName}`,
+      picture: photos[0].value,
+    };
+
+    done(null, user);
+  }
+}
---
+++ acms-be/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,47 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { User } from '../../user/entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JwtConfig } from 'src/config';
+
+export type JwtPayload = {
+  sub: string;
+  email: string;
+};
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private configService: ConfigService,
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {
+    const extractJwtFromCookie = (req) => {
+      let token = null;
+      if (req && req.cookies) {
+        token = req.cookies['access_token'];
+      }
+
+      return token || ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+    };
+
+    super({
+      ignoreExpiration: false,
+      secretOrKey: configService.get<JwtConfig>('jwt').secret,
+      jwtFromRequest: extractJwtFromCookie,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.userModel.findOne({ email: payload.sub });
+
+    if (!user) throw new UnauthorizedException('auth_error');
+
+    return {
+      id: payload.sub,
+      email: payload.email,
+    };
+  }
+}
