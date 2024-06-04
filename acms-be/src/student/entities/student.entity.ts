442c52843e9fd1c4e4bbc56cbe1b2f36c26e5605
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-04 16:02:19 +0300

    feat: add student entity

---
+++ acms-be/src/student/entities/student.entity.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type StudentDocument = HydratedDocument<Student>;
+
+@Schema()
+export class Student {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  group: string;
+
+  @Prop({ type: String, required: true })
+  codeforcesHandle: string;
+}
+
+export const StudentSchema = SchemaFactory.createForClass(Student);
