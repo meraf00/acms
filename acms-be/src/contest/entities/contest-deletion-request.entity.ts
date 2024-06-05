2cee3f142d99a9c867ee041a187045f4db16cc98
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-05 23:48:44 +0300

    feat: add contest deletion request entity

---
+++ acms-be/src/contest/entities/contest-deletion-request.entity.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+import { User } from 'src/user/entities/user.entity';
+import { Contest } from './contest.entity';
+
+export type ContestDeletionRequestDocument =
+  HydratedDocument<ContestDeletionRequest>;
+
+@Schema()
+export class ContestDeletionRequest {
+  @Prop({ type: Types.ObjectId, required: true, ref: Contest.name })
+  contest: Contest;
+
+  @Prop({ type: [Types.ObjectId], required: true, ref: User.name, default: [] })
+  approvals: User[];
+}
+
+export const ContestDeletionRequestSchema = SchemaFactory.createForClass(
+  ContestDeletionRequest,
+);
