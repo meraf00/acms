4eb8aa69afd6551a18b17a1f5a8501bf8cc231c0
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-05 23:48:02 +0300

    feat: add contest deletion service

---
+++ acms-be/src/contest/services/contest-deletion.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, UpdateWriteOpResult } from 'mongoose';
+import { EntityService } from 'src/shared/services/entity.service';
+import { ContestDeletionRequest } from '../entities/contest-deletion-request.entity';
+
+@Injectable()
+export class ContestDeletionRequestService extends EntityService<ContestDeletionRequest>(
+  ['contest'],
+) {
+  constructor(
+    @InjectModel(ContestDeletionRequest.name)
+    private readonly contestDeletionRequestModel: Model<ContestDeletionRequest>,
+  ) {
+    super(contestDeletionRequestModel);
+  }
+
+  async createDeletionRequest(
+    contestId: string,
+    userId: string,
+  ): Promise<ContestDeletionRequest> {
+    return this.create({
+      contest: contestId,
+      approvals: [userId],
+    });
+  }
+
+  async updateApproval(
+    deletionRequestId: string,
+    approve: boolean,
+    userId: string,
+  ): Promise<ContestDeletionRequest | null> {
+    let result: UpdateWriteOpResult;
+
+    if (approve) {
+      result = await this.update(deletionRequestId, {
+        $addToSet: { approvals: userId },
+      });
+    } else {
+      result = await this.update(deletionRequestId, {
+        $pull: { approvals: userId },
+      });
+    }
+
+    if (result.matchedCount === 0) throw new NotFoundException('not_found');
+
+    return await this.findOne(deletionRequestId);
+  }
+}
