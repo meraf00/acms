1dc0439b8e576d6adfe2cda452fb4e88f8377242
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-05 18:38:16 +0300

    feat: add image kit

--- acms-be/src/image-kit/image-kit.module.ts
+++ acms-be/src/image-kit/image-kit.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import ImageKit from 'imagekit';
+import { ImageKitConfig } from 'src/config';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: ImageKit,
+      useFactory: (configService: ConfigService) =>
+        new ImageKit({
+          publicKey: configService.get<ImageKitConfig>('imageKit')!.publicKey,
+          privateKey: configService.get<ImageKitConfig>('imageKit')!.privateKey,
+          urlEndpoint:
+            configService.get<ImageKitConfig>('imageKit')!.urlEndpoint,
+        }),
+      inject: [ConfigService],
+    },
+  ],
+})
 export class ImageKitModule {}
