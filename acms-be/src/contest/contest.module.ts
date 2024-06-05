9bb0375ad2abc36d99c4892574302fec4acc79da
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-05 23:49:51 +0300

    feat: register contest deletion controller

--- acms-be/src/contest/contest.module.ts
+++ acms-be/src/contest/contest.module.ts
@@ -4,12 +4,17 @@ import { ContestService } from './services/contest.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Contest, ContestSchema } from './entities/contest.entity';
 import { ActiveContestController } from './controllers/active-contest.controller';
+import { ContestDeletionRequestController } from './controllers/contest-deletion-request.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Contest.name, schema: ContestSchema }]),
   ],
-  controllers: [ContestController, ActiveContestController],
+  controllers: [
+    ContestController,
+    ActiveContestController,
+    ContestDeletionRequestController,
+  ],
   providers: [ContestService],
 })
 export class ContestModule {}
