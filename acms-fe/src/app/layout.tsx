027a76ae392ffa148a2f2ab0bed92dbac5309562
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-13 02:03:29 +0300

    feat: setup next with shadcn

---
+++ acms-fe/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Quicksand } from 'next/font/google';
+import './globals.css';
+import Providers from './providers';
+
+const quicksand = Quicksand({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'ACMS',
+  description: 'A2SV contest monitoring system',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={quicksand.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
