19f83f747f05dfb689620d4eade8320a75494d7f
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-14 00:57:22 +0300

    feat: add packages

--- acms-fe/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-import path from 'path';
-
-const configFile =
-  process.env.NODE_ENV === 'production' ? 'acms.production.json' : 'acms.json';
-
-const file = fs.readFileSync(path.join(process.cwd(), configFile), 'utf8');
-
-export const siteConfig = JSON.parse(file);
--- acms-fe/tailwind.config.ts
+++ acms-fe/tailwind.config.ts
@@ -11,7 +11,6 @@ const {
 const config = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
