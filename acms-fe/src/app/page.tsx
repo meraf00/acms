788e17668d56d6c065325aae9c3016b1b7f9f038
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-13 03:40:53 +0300

    feat: add theme providers to providers hierarchy

--- acms-fe/src/app/page.tsx
+++ acms-fe/src/app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { siteConfig } from '@/config';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+import { Icons } from '@/components/ui/icons';
+import { BackgroundBeams } from '@/components/ui/background-beams';
+
+export default function IndexPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world
-    </main>
+    <div className="container relative flex flex-col-reverse md:flex-row justify-center h-screen gap-y-16">
+      <div className="w-full -z-10 p-5 flex justify-center md:w-1/2 md:h-full md:flex-col-reverse md:justify-start">
+        <BackgroundBeams />
+        <div className="text-sm text-gray-400">Powered by A2SV.</div>
+      </div>
+      <div className="w-full flex flex-col gap-10 justify-center items-center md:w-1/2">
+        <Image
+          src={'/logos/acms-high-resolution-logo-white-transparent.svg'}
+          width={200}
+          height={200}
+          alt="ACMS logo"
+        ></Image>
+        <Link
+          href={siteConfig.links.google}
+          className={cn(buttonVariants({ variant: 'outline' }))}
+        >
+          <Icons.google className="h-4 w-4 mr-5" /> Continue with A2SV email
+        </Link>
+      </div>
+    </div>
   );
 }
