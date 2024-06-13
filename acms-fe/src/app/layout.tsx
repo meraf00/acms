29af981680855abfcef2b030ffccfc17dca6d34f
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-13 12:00:05 +0300

    feat: update theme toggle button

--- acms-fe/src/app/layout.tsx
+++ acms-fe/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Quicksand } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
+import { ModeToggle } from '@/components/ui/mode-toggle';
+import { cn } from '@/lib/utils';
 
 const quicksand = Quicksand({ subsets: ['latin'] });
 
@@ -17,8 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={quicksand.className}>
-        <Providers>{children}</Providers>
+      <body className={cn(quicksand.className, 'relative')}>
+        <Providers>
+          <div className="fixed bottom-5 right-5 z-50">
+            <ModeToggle />
+          </div>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- acms-fe/src/app/page.tsx
+++ acms-fe/src/app/page.tsx
@@ -1,33 +1,13 @@
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
 
-import { siteConfig } from '@/config';
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
-import { Icons } from '@/components/ui/icons';
-import { BackgroundBeams } from '@/components/ui/background-beams';
+import AuthGuard from '@/lib/features/auth/components/auth-guard';
 
-export default function IndexPage() {
+function IndexPage() {
   return (
     <div className="container relative flex flex-col-reverse md:flex-row justify-center h-screen gap-y-16">
-      <div className="w-full -z-10 p-5 flex justify-center md:w-1/2 md:h-full md:flex-col-reverse md:justify-start">
-        <BackgroundBeams />
-        <div className="text-sm text-gray-400">Powered by A2SV.</div>
-      </div>
-      <div className="w-full flex flex-col gap-10 justify-center items-center md:w-1/2">
-        <Image
-          src={'/logos/acms-high-resolution-logo-white-transparent.svg'}
-          width={200}
-          height={200}
-          alt="ACMS logo"
-        ></Image>
-        <Link
-          href={siteConfig.links.google}
-          className={cn(buttonVariants({ variant: 'outline' }))}
-        >
-          <Icons.google className="h-4 w-4 mr-5" /> Continue with A2SV email
-        </Link>
-      </div>
+      Hi there
     </div>
   );
 }
+
+export default AuthGuard({ Component: IndexPage });
