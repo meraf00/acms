e4c6cc7450e37d4381eef48b945b4bef3e2e2f3f
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-14 13:55:04 +0300

    feat: add staletime and get contest by id

--- acms-fe/src/lib/features/contest/api.ts
+++ acms-fe/src/lib/features/contest/api.ts
@@ -1,9 +1,22 @@
 import { AxiosInstance } from 'axios';
 import { Contest } from './types/contest';
 
+export interface GetContestParams {
+  id?: string;
+}
+
 export const getContests = async (
   client: AxiosInstance
 ): Promise<Contest[]> => {
   const { data } = await client.get('/contests');
-  return data;
+  return data.data;
+};
+
+export const getContest = async (
+  client: AxiosInstance,
+  params: GetContestParams
+): Promise<Contest[]> => {
+  const { id } = params;
+  const { data } = await client.get(`/contests/${id}`);
+  return data.data;
 };
--- acms-fe/src/lib/features/contest/hooks.ts
+++ acms-fe/src/lib/features/contest/hooks.ts
@@ -3,8 +3,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { useApi } from '@lib/core/hooks';
 import { getContests } from './api';
+import { siteConfig } from '@/lib/core/config';
 
 const QUERY_KEY = ['contests'];
+const staleTime = siteConfig.api.staleTime;
 
 export const useGetContests = () => {
   const client = useApi();
@@ -12,5 +14,16 @@ export const useGetContests = () => {
   return useQuery({
     queryKey: QUERY_KEY,
     queryFn: () => getContests(client),
+    staleTime,
+  });
+};
+
+export const useGetContest = (id: string) => {
+  const client = useApi();
+
+  return useQuery({
+    queryKey: [...QUERY_KEY, id],
+    queryFn: () => getContests(client),
+    staleTime,
   });
 };
