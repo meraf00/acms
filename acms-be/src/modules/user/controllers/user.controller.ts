9bfb5fb9264600acf1bda317613c1065b168ccda
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-17 18:44:07 +0300

    feat(acms-be): add tag to user controller

--- acms-be/src/modules/user/controllers/user.controller.ts
+++ acms-be/src/modules/user/controllers/user.controller.ts
@@ -1,13 +1,14 @@
 import { JwtAuthGuard } from '@modules/auth/guards/jwt-auth.guard';
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { Model } from 'mongoose';
 
 import { User, UserDocument } from '../entities/user.entity';
 
 @ApiBearerAuth()
+@ApiTags('auth')
 @Controller('users')
 @UseGuards(JwtAuthGuard)
 export class UserController {
