da975f2a01688b55dc49d38f0dabd5f05e6ffd15
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-17 18:18:14 +0300

    refactor(acms-be): move storage module

--- acms-be/src/app.module.ts
+++ acms-be/src/app.module.ts
@@ -1,12 +1,12 @@
 import { AuthModule } from '@modules/auth/auth.module';
 import { ContestModule } from '@modules/contest/contest.module';
+import { StorageModule } from '@modules/storage/storage.module';
 import { StudentModule } from '@modules/student/student.module';
 import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import config, { DatabaseConfig } from '@shared/config';
-import { StorageModule } from '@shared/modules/storage/storage.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
