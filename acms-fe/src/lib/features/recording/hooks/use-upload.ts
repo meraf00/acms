48f65726f23f65676cf1357372e35e181b96124f
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-18 10:03:24 +0300

    feat(acms-fe): add retry logic

--- acms-fe/src/lib/features/recording/hooks/use-upload.ts
+++ acms-fe/src/lib/features/recording/hooks/use-upload.ts
@@ -1,25 +1,24 @@
-import { incrementCapturedCount } from '../store/slice';
+import { retirableReq } from '@/lib/core/utils/retirable-req';
+import {
+  incrementCapturedScreenCount,
+  incrementCapturedCameraCount,
+} from '../store/slice';
 import { extractImageBlob } from './photo-util';
 import { useApi, useAppDispatch, useAppSelector } from '@/lib/core/hooks';
 
 export const useUpload = () => {
   const client = useApi();
 
-  const count = useAppSelector((state) => state.monitoring.capturedCount);
+  const screenCount = useAppSelector(
+    (state) => state.monitoring.capturedScreenCount
+  );
+  const cameraCount = useAppSelector(
+    (state) => state.monitoring.capturedScreenCount
+  );
   const dispatch = useAppDispatch();
 
-  async function upload(
-    cameraElement: HTMLVideoElement | null,
-    screenElement: HTMLVideoElement | null
-  ) {
-    if (!cameraElement || !screenElement) return;
-
-    const cameraImage = await extractImageBlob(cameraElement);
-    const screenImage = await extractImageBlob(screenElement);
-
-    if (!screenImage) return;
-
-    const fileData = new File([screenImage], `screen-${count}.jpg`, {
+  async function uploadBlob(blob: Blob, prefix: string, count: number) {
+    const fileData = new File([blob], `${prefix}-${count}.jpg`, {
       type: 'image/jpeg',
     });
 
@@ -32,19 +31,49 @@ export const useUpload = () => {
 
     const presignedUrl = response.data.data;
 
-    console.log('Uploading image...');
-    console.log(response.data.data);
-    const uploadResponse = await client.put(
-      'http://localhost:8333/a2sv-cms-images/screen-7-1718655702492-21a1f57e-5095-4f3b-a5e4-ea16b8f1ddc0.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=access-key%2F20240617%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240617T202142Z&X-Amz-Expires=100&X-Amz-Signature=87fe98b1a06581ff0db8a710f7b2d3bc355b1ebb0d196236624d6917342aa853&X-Amz-SignedHeaders=host&x-id=PutObject',
-      fileData,
-      {
-        withCredentials: false,
-      }
+    console.log(`Uploading ${prefix}...`);
+    const uploadResponse = await retirableReq(
+      async () =>
+        await fetch(presignedUrl, {
+          method: 'PUT',
+          body: fileData,
+        })
     );
 
-    if (uploadResponse.status !== 200) return;
-    dispatch(incrementCapturedCount());
+    if (uploadResponse?.status !== 200) return;
     console.log('Upload successful!');
+    return true;
+  }
+
+  async function upload(
+    cameraElement: HTMLVideoElement | null,
+    screenElement: HTMLVideoElement | null
+  ) {
+    if (screenElement) {
+      const screenImage = await extractImageBlob(screenElement);
+      if (screenImage) {
+        const screenUploadOk = await uploadBlob(
+          screenImage,
+          'screen',
+          screenCount
+        );
+        if (screenUploadOk) {
+          dispatch(incrementCapturedScreenCount());
+        }
+      }
+    }
+
+    if (cameraElement) {
+      const cameraImage = await extractImageBlob(cameraElement);
+      if (cameraImage) {
+        const cameraUploadOk = await uploadBlob(
+          cameraImage,
+          'camera',
+          cameraCount
+        );
+        if (cameraUploadOk) dispatch(incrementCapturedCameraCount());
+      }
+    }
   }
 
   return upload;
