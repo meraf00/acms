288677b3709d1b434155be60524cce1d22bd8b70
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-23 20:27:23 +0300

    feat(acms-fe): remove ongoing from sidebar

--- acms-fe/src/lib/features/dashboard/components/side-bar.tsx
+++ acms-fe/src/lib/features/dashboard/components/side-bar.tsx
@@ -53,14 +53,6 @@ export function Sidebar({ className }: any) {
                 Ongoing
               </Link>
 
-              {/* <Link
-                href="/contests/upcoming"
-                className={activateOn('/contests/upcoming')}
-              >
-                <Timer className="mr-2 h-4 w-4" />
-                Upcoming
-              </Link> */}
-
               <Link
                 href="/contests/add"
                 className={activateOn('/contests/add')}
