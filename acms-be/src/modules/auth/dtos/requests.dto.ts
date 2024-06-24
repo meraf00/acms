2705ddb3cef3f208e8c393e6447f5dbc85efc701
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-24 15:41:28 +0300

    feat(acms-be): rename user dto

--- acms-be/src/modules/auth/dtos/requests.dto.ts
+++ acms-be/src/modules/auth/dtos/requests.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail } from 'class-validator';
 
-export class RegisterUserDto {
+export class UserDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 }
