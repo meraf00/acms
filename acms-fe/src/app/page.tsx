48a2b7c4ef99f1a93db7e3ea4b98f6eae356646a
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-06-25 15:40:50 +0300

    feat(acms-fe): add guides

--- acms-fe/src/app/page.tsx
+++ acms-fe/src/app/page.tsx
@@ -1,5 +1,46 @@
+import { Guides } from '@/lib/features/guides/guides';
+import Image from 'next/image';
+import DarkLogo from '@public/logos/acms-high-resolution-logo-white-transparent.svg';
+import LightLogo from '@public/logos/acms-high-resolution-logo-transparent.svg';
+import Link from 'next/link';
+import { cn } from '@/lib/core/utils';
+import { buttonVariants } from '@/components/ui/button';
+import { Activity } from 'lucide-react';
+
 export default function Page() {
   return (
-    <div className="min-h-screen flex justify-center items-center gap-5 flex-wrap p-5"></div>
+    <div className="min-h-screen flex flex-col gap-10 flex-wrap px-24 py-16 ">
+      <div className="flex justify-between items-center">
+        <Image
+          className="block dark:hidden"
+          src={LightLogo}
+          width={200}
+          height={200}
+          alt="ACMS logo"
+        />
+        <Image
+          className="hidden dark:block"
+          src={DarkLogo}
+          width={200}
+          height={200}
+          alt="ACMS logo"
+        />
+
+        <div className="flex mt-10">
+          <Link
+            href="/contests/ongoing"
+            className={cn(
+              'w-full',
+              'gap-2',
+              buttonVariants({ variant: 'link' })
+            )}
+          >
+            <Activity className="w-4 h-4" />
+            Ongoing Contests
+          </Link>
+        </div>
+      </div>
+      <Guides />
+    </div>
   );
 }
---
+++ acms-fe/src/lib/features/guides/guides.tsx
@@ -0,0 +1,48 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+export function Guides() {
+  return (
+    <div className="flex flex-col gap-5">
+      <h1 className="font-semibold text-3xl">Guides</h1>
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full flex flex-col gap-5"
+      >
+        <AccordionItem value="item-1">
+          <AccordionTrigger>How to login?</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-2">
+            <p>
+              To access ACMS, you can use your A2SV account. If you are already
+              signed in to your A2SV Google account, simply follow the Google
+              authentication flow to log in.
+            </p>
+            <p>
+              Alternatively, you can request a login link, which will be sent to
+              your A2SV email, and open it in your preferred browser.
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>How to start monitoring?</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-2">
+            <p>
+              Visit acms.vercel.app/ongoing. You will see a list of ongoing
+              contests. Click on{' '}
+              <span className="font-bold">&quot;Start Monitoring&quot;</span>{' '}
+              next to the contest you want to join.
+            </p>
+            <p>
+              Make sure to allow camera and screen permissions when prompted.
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
