33f9ca0e31197b772c2b38ce3a8275f021e9606a
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2024-08-27 22:39:30 +0300

    refactor(acms-fe): organize common utils

---
+++ acms-fe/src/lib/formatters.ts
@@ -0,0 +1,11 @@
+export const formatDate = (dateString: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+  return new Date(dateString).toLocaleString(undefined, options);
+};
